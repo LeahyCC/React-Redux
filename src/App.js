09d081dee8f0680a14095f3b1843f982a66b36a5
Colin Leahy <ccleahy2020@gmail.com>
2018-05-09 12:33:12 -0500

    new lession

--- src/App.js
+++ src/App.js
@@ -1,68 +1,29 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Provider } from 'react-redux';
 
-import { connect } from 'react-redux';
-import { updateUser, apiRequest } from './actions/user-action';
-import { createSelector } from 'reselect';
+import Posts from './components/Posts';
+import PostForm from './components/Postform';
+
+import store from './store';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-
-    this.onUpdateUser = this.onUpdateUser.bind(this);
-  }
-
-  componentDidMount() {
-    this.props.onApiRequest();
-  }
-
-  onUpdateUser(event) {
-    this.props.onUpdateUser(event.target.value);
-  }
-
   render() {
-    console.log(this.props);
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>
-          <input onChange={this.onUpdateUser} />
-          {this.props.user}
-        </p>
-      </div>
+      <Provider store={store}>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">Welcome to React</h1>
+          </header>
+          <PostForm />
+          <hr/>
+          <Posts />
+        </div>
+      </Provider>
     );
   }
 }
 
-const productSelector = createSelector(
-  state => state.products,
-  products => products
-);
-
-const userSelector = createSelector(
-  state => state.user,
-  user => user
-);
-
-const mapStateToProps = createSelector(
-  productSelector,
-  userSelector,
-  (products, user) => ({
-    products,
-    user
-  })
-);
-
-const mapActionsToProps = {
-  onUpdateUser: updateUser,
-  onApiRequest: apiRequest
-}
-
-export default connect(mapStateToProps, mapActionsToProps) (App);
+export default App;
--- src/index.js
+++ src/index.js
@@ -4,30 +4,5 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-import ReduxThunk from 'redux-thunk';
-import {  applyMiddleware, compose, combineReducers, createStore } from 'redux';
-import { Provider } from 'react-redux';
-
-import productReducer from './reducers/product-reducer'
-import userReducer from './reducers/user-reducer'
-
-const allReducers = combineReducers({
-    products: productReducer,
-    user: userReducer
-});
-
-const allStoreEnhancers = compose(
-    applyMiddleware(ReduxThunk),
-    window.devToolsExtension && window.devToolsExtension()
-);
-
-const store = createStore(allReducers,
-    {
-        products: [{ name: 'iPhone'}],
-        user: 'Colin'
-    },
-    allStoreEnhancers
-);
-
-ReactDOM.render(<Provider store={store}><App aRandomProp="Foo" /></Provider>, document.getElementById('root'));
+ReactDOM.render(<App aRandomProp="Foo" />, document.getElementById('root'));
 registerServiceWorker();
